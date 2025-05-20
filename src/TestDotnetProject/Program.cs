111356ae5b8f9edaadb7da2ce6f88367282a3355
gri-gri <46087070+gri-gri@users.noreply.github.com>
2025-05-20 12:56:04 +0400

    feat: change in-memory db on postgresql

--- src/TestDotnetProject/Program.cs
+++ src/TestDotnetProject/Program.cs
@@ -6,7 +6,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.Configure<RouteOptions>(options => options.LowercaseUrls = true);
 builder.Services.AddControllers();
 
-builder.Services.AddDbContext<UsersDbContext>(options => options.UseInMemoryDatabase("Users"));
+builder.Services.AddDbContext<UsersDbContext>(options => options.UseNpgsql(
+    "Host=db;Port=5432;Database=app;Username=admin;Password=admin"));
 
 builder.Services.AddScoped<UsersRepository, UsersRepository>();
 
@@ -14,6 +15,13 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddOpenApiDocument();
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<UsersDbContext>();
+    await db.Database.EnsureDeletedAsync();
+    await db.Database.EnsureCreatedAsync();
+}
 
 if (app.Environment.IsDevelopment())
 {
